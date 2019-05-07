a74cb986dabc0b4ae20ac9eebce9307b48027b26
Emilia Ziemichód <emifrompoland@gmail.com>
2019-05-07 15:42:16 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,12 +2,10 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-end
-class Kid
   include Dance
-
+ 
   attr_accessor :name
-
+ 
   def initialize(name)
     @name = name
   end
